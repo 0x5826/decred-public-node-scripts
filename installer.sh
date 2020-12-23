883024016c5e9979a677805558c85799df4ab6bc
Dante-Z <dante@0x5826.one>
2020-12-23 12:12:33 +0800

    update

--- installer.sh
+++ installer.sh
@@ -23,6 +23,13 @@ TARBALL="decred-linux-$MACHINE-v$VERSION.tar.gz"
 DOWNLOADURL="https://github.com/decred/decred-binaries/releases/download/v$VERSION/$TARBALL"
 SERVICEURL="https://raw.githubusercontent.com/0x5826/decred-public-node-scripts/main/systemd/dcrd.service"
 TMPDIR="$(mktemp -d)"
+USER="dcrd"
+GROUP="dcrd"
+DCRD_USER_HOME="/home/$USER"
+DCRD_DATA_HOME="/home/$USER/.dcrd"
+BINARYDIR="$DCRD_USER_HOME/decred"
+BINARYPATH="$DCRD_USER_HOME/decred/dcrd"
+CONFIGPATH="$DCRD_USER_HOME/.dcrd/dcrd.conf"
 
 function check_environment() {
   egrep "^$GROUP" /etc/group >& /dev/null
@@ -103,14 +110,7 @@ function download_dcrd() {
 
 function install_dcrd() {
   echo "[INFO]Installing dcrd……"
-  USER="dcrd"
-  GROUP="dcrd"
-  DCRD_USER_HOME="/home/$USER"
-  DCRD_DATA_HOME="/home/$USER/.dcrd"
-  BINARYDIR="$DCRD_USER_HOME/decred"
-  BINARYPATH="$DCRD_USER_HOME/decred/dcrd"
-  CONFIGPATH="$DCRD_USER_HOME/.dcrd/dcrd.conf"
-
+# Step 1 create dcrd dcrd
   egrep "^$GROUP" /etc/group >& /dev/null
   if [ $? -ne 0 ]
   then
@@ -122,11 +122,12 @@ function install_dcrd() {
   then
       useradd -m -g $GROUP -s /sbin/nologin $USER
   fi
-
+# Step 2 COPY binary and systemd service file
   mkdir -p $DCRD_DATA_HOME && chown dcrd:dcrd $DCRD_DATA_HOME
   mkdir -p $BINARYDIR && chown -R dcrd:dcrd $BINARYDIR
-  cp -f "/tmp/$TMPDIR/decred-linux-$MACHINE-v$VERSION/dcrd" $BINARYPATH && chown dcrd:dcrd $BINARYPATH &&chmod a+x $BINARYPATH
-  cp -f "/tmp/$TMPDIR/dcrd.service" /etc/systemd/system/dcrd.service
+  cp -f "$TMPDIR/decred-linux-$MACHINE-v$VERSION/dcrd" $BINARYPATH && chown dcrd:dcrd $BINARYPATH &&chmod a+x $BINARYPATH
+  cp -f "$TMPDIR/dcrd.service" /etc/systemd/system/dcrd.service
+  
   read -p "[INFO]Start dcrd at boot: [Y/n] " yn
     case $yn in
         [Yy] )
@@ -136,17 +137,40 @@ function install_dcrd() {
         echo "[INFO]You can use command 'systemd enable dcrd.service' to enable it later."
         ;;
     esac
-  
+# Step 3 Generate dcrd.conf
   echo "[INFO]Generating dcrd.conf……"
   echo "externalip=$INTERNET_IPv4" >> $CONFIGPATH
-  echo "[INFO]Starting dcrd node program……"
 
+# Step 4 Run dcrd.conf
+  echo "[INFO]Running dcrd node program……"
   systemd start dcrd.service
   dcrd_status=$(systemctl status dcrd.service)
   if [[ $str =~ "running" ]]
   then
       echo "[INFO]dcrd is running, Clean tmp files……"
-      rm -rf "/tmp/$TMPDIR"
+      rm -rf "$TMPDIR"
+  else
+      echo "[ERROR]There is something wrong with running dcrd. info:"
+      systemctl status dcrd.service
+      exit 1
+  fi
+}
+
+function upgrade_dcrd() {
+  echo "[INFO]stop dcrd node program……"
+  systemctl stop dcrd.service
+  cd "$TMPDIR"
+  echo "[INFO]Downloading $TARBALL..."
+  curl -LO --progress-bar "$DOWNLOADURL" || wget -q --show-progress "$DOWNLOADURL"
+  echo "[INFO]Unpacking $TARBALL..."
+  tar zxf "$TARBALL"
+  cp -f "$TMPDIR/decred-linux-$MACHINE-v$VERSION/dcrd" $BINARYPATH && chown dcrd:dcrd $BINARYPATH &&chmod a+x $BINARYPATH
+  echo "[INFO]starting dcrd node program……"
+  dcrd_status=$(systemctl status dcrd.service)
+  if [[ $str =~ "running" ]]
+  then
+      echo "[INFO]dcrd is running, Clean $TMPDIR……"
+      rm -rf "$TMPDIR"
   else
       echo "[ERROR]There is something wrong with running dcrd. info:"
       systemctl status dcrd.service
@@ -155,9 +179,12 @@ function install_dcrd() {
 }
 
 function uninstall_dcrd() {
-
-}
-
-function upgrade_dcrd() {
-  
+  echo "[INFO]stop dcrd node program……"
+  systemctl stop dcrd.service
+  echo "[INFO]disable dcrd node program on boot and delete dcrd.service……"
+  systemctl disable dcrd.service
+  rm -f /etc/systemd/system/dcrd.service
+  echo "[INFO]Delete USER:dcrd/GROUP:dcrd/dcrd's home."
+  userdel -r $USER
+  groupdel $GROUP
 }
